fe512128c50420304c37ac4ea04c1c9424a76053
Edwin Bratini <edwin.bratini@gmail.com>
2015-11-09 13:54:52 +0000

    Finish Developing Hangperson Using TDD and Autotest

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,49 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  def guess(guess)
+    letter = guess.to_s.downcase[0]
+    raise ArgumentError, 'guess cannot be either: empty, nonletter or nil.' unless letter =~ /[a-z]/i
+    
+    # return false if already guessed
+    return false if self.guesses.include?(letter) || self.wrong_guesses.include?(letter)
+    
+    valid = self.word.include?(letter)
+    if valid
+      self.guesses << letter
+    else
+      self.wrong_guesses << letter
+    end
+  end
+  
+  def check_win_or_lose
+    # if self.wrong_guesses.size < 7
+    #   if self.word_with_guesses.eql?(self.word)
+    #     :win
+    #   else
+    #     :play
+    #   end
+    # else
+    #   :lose
+    # end
+    
+    return :lose unless self.wrong_guesses.size < 7
+    self.wrong_guesses.size < 7 && self.word_with_guesses.eql?(self.word) ? :win : :play
+  end
+  
+  def word_with_guesses
+    return self.word.gsub(/./, '-') if self.guesses.empty?
+    
+    gmatch_rgx = /[^#{ self.guesses }]/i
+    self.word.gsub(gmatch_rgx, '-')
   end
 
   def self.get_random_word
@@ -18,5 +59,4 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
 end
